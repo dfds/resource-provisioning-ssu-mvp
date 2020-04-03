9e2f864bdf5914304580e7658656478407924768
toban@dfds.com <toban@dfds.com>
2020-04-03 13:10:40 +0200

    .

--- src/ResourceProvisioning.Abstractions/Infrastructure/GridActorType.cs
+++ src/ResourceProvisioning.Abstractions/Infrastructure/GridActorType.cs
@@ -3,8 +3,9 @@
 namespace ResourceProvisioning.Abstractions.Infrastructure
 {
 	[Flags]
-	public enum GridActorType 
+	public enum GridActorType
 	{
+		None = 0,
 		User = 1,
 		System = 2,
 		External = 4
