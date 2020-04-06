ac7aa7edb9416d8d5c527be8765c499097e0f900
toban@dfds.com <toban@dfds.com>
2020-04-06 14:06:24 +0200

    .

--- src/ResourceProvisioning.Broker.Host.Api/Controllers/V1/ControlPlaneController.cs
+++ src/ResourceProvisioning.Broker.Host.Api/Controllers/V1/ControlPlaneController.cs
@@ -6,11 +6,11 @@ namespace ResourceProvisioning.Broker.Host.Api.Controllers.V1
 {
 	[ApiController]
 	[Route("[controller]")]
-	public class EnvironmentController : ControllerBase
+	public class ControlPlaneController : ControllerBase
 	{
 		private readonly IMediator _mediator;
 
-		public EnvironmentController(IMediator mediator)
+		public ControlPlaneController(IMediator mediator)
 		{
 			_mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
 		}
