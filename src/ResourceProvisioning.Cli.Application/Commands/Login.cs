ea6ea5daf86d1ac5f904417d76ac21680876d3c2
Emil H. Clausen <sequoiia@hummel.yt>
2020-06-12 13:21:44 +0200

    Split up OAuth login flows for CLI

--- src/ResourceProvisioning.Cli.Application/Commands/Login.cs
+++ src/ResourceProvisioning.Cli.Application/Commands/Login.cs
@@ -1,10 +1,9 @@
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using McMaster.Extensions.CommandLineUtils;
-using Newtonsoft.Json;
+using ResourceProvisioning.Cli.Core.Core.Authentication;
 
 namespace ResourceProvisioning.Cli.Application.Commands
 {
@@ -13,116 +12,64 @@ namespace ResourceProvisioning.Cli.Application.Commands
 	public sealed class Login : CliCommand
 	{
 		private HttpClient _httpClient;
+
+		[Option(CommandOptionType.NoValue, LongName = "devicecode", ShortName = "dc")]
+		public bool DeviceCode { get; }
+		[Option(CommandOptionType.NoValue, LongName = "interactive", ShortName = "int")]
+		public bool Interactive { get; }
+		[Option(CommandOptionType.NoValue, LongName = "authorization", ShortName = "aut")]
+		public bool Authorization { get; }
 		
 		public Login()
 		{
 			_httpClient = new HttpClient();
 		}
 
-		private async Task<DeviceCodeResponse> InitialiseFlow()
+		private int AmountOfAuthOptionsSelected()
 		{
-			var dict = new Dictionary<string, string>();
-			// TODO: Don't hardcode client_id 
-			dict.Add("client_id", "72d0443b-ff34-4568-8eb9-1d81849c5462");
-			dict.Add("scope", "user.read openid profile");
-			// TODO: Don't hardcode URI
-			var resp = await _httpClient.PostAsync("https://login.microsoftonline.com/73a99466-ad05-4221-9f90-e7142aa2f6c1/oauth2/v2.0/devicecode", new FormUrlEncodedContent(dict));
-			var respPayload = await resp.Content.ReadAsStringAsync();
-			
-			var deviceCodeResponse = JsonConvert.DeserializeObject<DeviceCodeResponse>(respPayload);
-			return deviceCodeResponse;
+			var count = 0;
+			count = DeviceCode ? count + 1 : count;
+			count = Interactive ? count + 1 : count;
+			count = Authorization ? count + 1 : count;
+
+			return count;
 		}
 		
 		public async override Task<int> OnExecuteAsync(CancellationToken cancellationToken = default)
 		{
-			var response = await InitialiseFlow();
-			Console.WriteLine(response.Message);
-			var tokenResponse = await Poll(response);
-			Console.WriteLine(tokenResponse.IdToken);
+			if (AmountOfAuthOptionsSelected() > 1)
+			{
+				Console.WriteLine("Too many methods of login has been selected. Only one of either 'devicecode', 'interactive' or 'authorization' may be selected at any time.");
+				return 1;
+			}
+
+			if (AmountOfAuthOptionsSelected() == 0)
+			{
+				var deviceCode = new DeviceCode();
+				await deviceCode.Auth();
+				Console.WriteLine(DeviceCode);
+				return 0;
+			}
+
+			if (DeviceCode)
+			{
+				var deviceCode = new DeviceCode();
+				await deviceCode.Auth();
+				Console.WriteLine(DeviceCode);
+				return 0;
+			}
+
+			if (Interactive)
+			{
+				throw new NotImplementedException();
+			}
+
+			if (Authorization)
+			{
+				throw new NotImplementedException();
+			}
+
 			return 0;
 		}
-
-		private async Task<TokenValidResponse> Poll(DeviceCodeResponse deviceCodeResponse)
-		{
-			var dict = new Dictionary<string, string>();
-			// TODO: Don't hardcode client_id 
-			dict.Add("client_id", "72d0443b-ff34-4568-8eb9-1d81849c5462");
-			dict.Add("grant_type", "urn:ietf:params:oauth:grant-type:device_code");
-			dict.Add("device_code", deviceCodeResponse.DeviceCode);
-			
-			while (true)
-			{
-				// TODO: Don't hardcode URI
-				var resp = await _httpClient.PostAsync("https://login.microsoftonline.com/73a99466-ad05-4221-9f90-e7142aa2f6c1/oauth2/v2.0/token", new FormUrlEncodedContent(dict));
-				var respPayload = await resp.Content.ReadAsStringAsync();
-
-				if (resp.IsSuccessStatusCode)
-				{
-					return JsonConvert.DeserializeObject<TokenValidResponse>(respPayload);
-				}
-
-				var error = JsonConvert.DeserializeObject<TokenErrorResponse>(respPayload);
-				if (error.Error.Equals("authorization_pending"))
-				{
-					//Console.WriteLine($"Sleeping {deviceCodeResponse.Interval}");
-				}
-				else
-				{
-					throw new Exception(error.ErrorDescription);
-				}
-
-				Thread.Sleep(deviceCodeResponse.Interval * 1000); // Wait the requested interval before polling again.
-			}
-		}
-	}
-
-	class DeviceCodeResponse
-	{
-		[JsonProperty("user_code")]
-		public string UserCode { get; set; }
-		[JsonProperty("device_code")]
-		public string DeviceCode { get; set; }
-		[JsonProperty("verification_uri")]
-		public string VerificationUri { get; set; }
-		[JsonProperty("expires_in")]
-		public int ExpiresIn { get; set; }
-		[JsonProperty("interval")]
-		public int Interval { get; set; }
-		[JsonProperty("message")]
-		public string Message { get; set; }
-	}
-
-	class TokenValidResponse // 200
-	{
-		[JsonProperty("token_type")]
-		public string TokenType { get; set; }
-		[JsonProperty("scope")]
-		public string Scope { get; set; }
-		[JsonProperty("access_token")]
-		public string AccessToken { get; set; }
-		[JsonProperty("id_token")]
-		public string IdToken { get; set; }
-		[JsonProperty("expires_in")]
-		public int ExpiresIn { get; set; }
-		[JsonProperty("ext_expires_in")]
-		public int ExtExpiresIn { get; set; }
-	}
-
-	class TokenErrorResponse // 400
-	{
-		[JsonProperty("error")]
-		public string Error { get; set; }
-		[JsonProperty("error_description")]
-		public string ErrorDescription { get; set; }
-		[JsonProperty("error_codes")]
-		public IEnumerable<int> ErrorCodes { get; set; }
-		[JsonProperty("timestamp")]
-		public string Timestamp { get; set; }
-		[JsonProperty("trace_id")]
-		public string TraceId { get; set; }
-		[JsonProperty("correlation_id")]
-		public string CorrelationId { get; set; }
-		[JsonProperty("error_uri")]
-		public string ErrorUri { get; set; }
 	}
 }
