06908ff35d38b4cb246d27008987d7469eeb76ab
Emil H. Clausen <sequoiia@hummel.yt>
2020-06-18 11:03:41 +0200

    Added UsernamePassword flow

---
+++ src/ResourceProvisioning.Cli.Application/Authentication/UsernamePasswordFlow.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ResourceProvisioning.Cli.Application.Authentication
+{
+	// Doesn't work with MFA. Intended for legacy ServiceAccount usage.
+	public class UsernamePasswordFlow : IAuthentication
+	{
+		private HttpClient _httpClient;
+
+		public UsernamePasswordFlow()
+		{
+			_httpClient = new HttpClient();
+		}
+
+		public async Task<AuthenticationToken> Auth()
+		{
+			var userData = GetUserData();
+			var response = await Login(userData);
+
+			return new AuthenticationToken
+			{
+				AccessToken = response.AccessToken,
+				IdToken = response.IdToken,
+				Scope = response.Scope,
+				ExpiresIn = response.ExpiresIn,
+				TokenType = response.TokenType
+			};
+		}
+
+		private UserData GetUserData()
+		{
+			var envUser = Environment.GetEnvironmentVariable("SSU_USER");
+			var envPass = Environment.GetEnvironmentVariable("SSU_PASSWORD");
+
+			if (envUser != null && envPass != null)
+			{
+				return new UserData
+				{
+					Username = envUser,
+					Password = envPass
+				};
+			}
+			
+			var userData = new UserData();
+			Console.Write("Username: ");
+			userData.Username = Console.ReadLine();
+			Console.Write("Password: ");
+			string password = null;
+			while (true)
+			{
+				var key = Console.ReadKey(true);
+				if (key.Key == ConsoleKey.Enter)
+					break;
+				password += key.KeyChar;
+			}
+			userData.Password = password;
+
+			Console.Write("\n");
+			return userData;
+		}
+
+		private async Task<InteractiveFlowTokenValidResponse> Login(UserData input)
+		{
+			var dict = new Dictionary<string, string>();
+			// TODO: Don't hardcode client_id 
+			dict.Add("client_id", "72d0443b-ff34-4568-8eb9-1d81849c5462");
+			dict.Add("scope", "user.read openid profile");
+			dict.Add("username", input.Username);
+			dict.Add("password", input.Password);
+			dict.Add("grant_type", "password");
+			// TODO: Don't hardcode URI
+			var resp = await _httpClient.PostAsync("https://login.microsoftonline.com/73a99466-ad05-4221-9f90-e7142aa2f6c1/oauth2/v2.0/token", new FormUrlEncodedContent(dict));
+			var respPayload = await resp.Content.ReadAsStringAsync();
+			
+			if (resp.IsSuccessStatusCode)
+			{
+				var response = JsonConvert.DeserializeObject<InteractiveFlowTokenValidResponse>(respPayload);
+				return response;
+			}
+			
+			Console.WriteLine(respPayload);
+			throw new Exception(respPayload);
+
+		}
+	}
+
+	class InteractiveFlowTokenValidResponse // 200
+	{
+		[JsonProperty("token_type")]
+		public string TokenType { get; set; }
+		[JsonProperty("scope")]
+		public string Scope { get; set; }
+		[JsonProperty("access_token")]
+		public string AccessToken { get; set; }
+		[JsonProperty("id_token")]
+		public string IdToken { get; set; }
+		[JsonProperty("expires_in")]
+		public int ExpiresIn { get; set; }
+		[JsonProperty("refresh_token")]
+		public int RefreshToken { get; set; }
+	}
+}
--- src/ResourceProvisioning.Cli.Application/Commands/Login.cs
+++ src/ResourceProvisioning.Cli.Application/Commands/Login.cs
@@ -19,6 +19,8 @@ namespace ResourceProvisioning.Cli.Application.Commands
 		public bool Interactive { get; }
 		[Option(CommandOptionType.NoValue, LongName = "authorization", ShortName = "aut")]
 		public bool Authorization { get; }
+		[Option(CommandOptionType.NoValue, LongName = "usernamepassword", ShortName = "up")]
+		public bool UsernamePassword { get; }
 		
 		public Login()
 		{
@@ -31,6 +33,7 @@ namespace ResourceProvisioning.Cli.Application.Commands
 			count = DeviceCode ? count +  1 : count;
 			count = Interactive ? count +  1 : count;
 			count = Authorization ? count +  1 : count;
+			count = UsernamePassword ? count + 1 : count;
 
 			return count;
 		}
@@ -62,6 +65,14 @@ namespace ResourceProvisioning.Cli.Application.Commands
 			if (Interactive)
 			{
 				var interactive = new InteractiveFlow();
+				var response = await interactive.Auth();
+				Console.WriteLine(response.IdToken);
+				return 0;
+			}
+			
+			if (UsernamePassword)
+			{
+				var interactive = new UsernamePasswordFlow();
 				var response = await interactive.Auth();
 				Console.WriteLine(response.IdToken);
 				return 0;
