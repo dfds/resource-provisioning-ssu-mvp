c7ebae0496601f3e572487313cbf31df6be5f39a
ZaradarDFDS <54835628+ZaradarDFDS@users.noreply.github.com>
2020-06-25 13:57:49 +0200

    - Removing "force-checkout" merge error (#20)

--- src/ResourceProvisioning.Broker.Host.Api/Startup.cs
+++ src/ResourceProvisioning.Broker.Host.Api/Startup.cs
@@ -7,7 +7,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 using ResourceProvisioning.Broker.Host.Api.Infrastructure.Authentication;
-using ResourceProvisioning.Broker.Host.Api.Infrastructure.Filters;
 using ResourceProvisioning.Broker.Infrastructure.EntityFramework;
 
 namespace ResourceProvisioning.Broker.Host.Api
@@ -62,8 +61,6 @@ namespace ResourceProvisioning.Broker.Host.Api
 					[nameof(DomainContext)] = "Filename=:memory:;"
 				};
 			});
-
-			services.AddTransient<IStartupFilter, DomainContextStartupFilter>();
 
 			ConfigureAuth(services);
 		}
