5d0c19dbd240dc617fe160ea2c2f1ccf25bdeb9f
Emil H. Clausen <sequoiia@hummel.yt>
2020-07-07 13:25:07 +0200

    Added CliApplicationOptions

---
+++ src/ResourceProvisioning.Cli.Application/CliApplicationOptions.cs
@@ -0,0 +1,8 @@
+namespace ResourceProvisioning.Cli.Application
+{
+	public sealed class CliApplicationOptions
+	{
+		public string Username { get; set; }
+		public string Password { get; set; }
+	}
+}
--- src/ResourceProvisioning.Cli.Application/DependencyInjection.cs
+++ src/ResourceProvisioning.Cli.Application/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using ResourceProvisioning.Cli.Domain.Services;
 using ResourceProvisioning.Cli.Infrastructure.Protocols.Http;
 
@@ -8,12 +10,24 @@ namespace ResourceProvisioning.Cli.Application
 	{
 		public static void AddCli(this IServiceCollection services)
 		{
+			var configuration = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddEnvironmentVariables()
+				.AddJsonFile("appsettings.json")
+				.Build();
+			
+			services.ConfigureCli(configuration);
 			services.AddServices();
 		}
 
 		private static void AddServices(this IServiceCollection services)
 		{
 			services.AddTransient<IBrokerService, BrokerClient>();
+		}
+
+		private static void ConfigureCli(this IServiceCollection services, IConfiguration configuration)
+		{
+			services.Configure<CliApplicationOptions>(configuration.GetSection("SsuCli:Authentication"));
 		}
 	}
 }
