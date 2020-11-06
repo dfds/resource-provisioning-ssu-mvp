9da5cedfb57844f7b58733dbcfc49dbf8d8e2445
toban@dfds.com <toban@dfds.com>
2020-11-06 12:18:35 +0100

    - Added IFacade interface

---
+++ src/ResourceProvisioning.Abstractions/Facade/IFacade.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using ResourceProvisioning.Abstractions.Commands;
+
+namespace ResourceProvisioning.Abstractions.Facade
+{
+	public interface IFacade
+	{
+		Task<T> Execute<T>(ICommand<T> command, CancellationToken cancellationToken = default);
+	}
+}
