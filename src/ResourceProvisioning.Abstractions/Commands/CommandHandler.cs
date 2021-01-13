d5bc2e2327daf6bdf25f1dfee97e11d98453c39a
Tobias Andersen <toban@dfds.com>
2021-01-13 08:47:47 +0100

    - Make mediator arg optional

--- src/ResourceProvisioning.Abstractions/Commands/CommandHandler.cs
+++ src/ResourceProvisioning.Abstractions/Commands/CommandHandler.cs
@@ -8,7 +8,7 @@ namespace ResourceProvisioning.Abstractions.Commands
 	{
 		protected readonly IMediator _mediator;
 
-		protected CommandHandler(IMediator mediator)
+		protected CommandHandler(IMediator mediator = default)
 		{
 			_mediator = mediator;
 		}
