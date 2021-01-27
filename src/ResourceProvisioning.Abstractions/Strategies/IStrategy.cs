aca21889984f9dbaad9b068795612ed9bf73f97f
Tobias Andersen <toban@dfds.com>
2021-01-27 11:10:37 +0100

    - Changed IStrategy to return void. If people want to manipulate the instance they are supplying to the strategy at runtime they can pass-by-reference (ref)

--- src/ResourceProvisioning.Abstractions/Strategies/IStrategy.cs
+++ src/ResourceProvisioning.Abstractions/Strategies/IStrategy.cs
@@ -1,10 +1,9 @@
 ﻿using System.Threading;
-using System.Threading.Tasks;
 
 namespace ResourceProvisioning.Abstractions.Strategies
 {
 	public interface IStrategy<T>
 	{
-		ValueTask<T> Apply(T target, CancellationToken cancellationToken = default);
+		void Apply(T target, CancellationToken cancellationToken = default);
 	}
 }
